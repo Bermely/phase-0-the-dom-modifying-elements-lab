7ee7b3ba2851546f0dcc5a656b99114c87792ec1
Bermely Ferreras <bfbferreras@gmail.com>
2023-10-02 13:44:20 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+mainElement.remove();
+const newHeader= document.createElement("h1")
+newHeader.id="victory";
+const yourName= "Bermely"; 
+newHeader.textContent = '${Bermely} is the champion';
+document.body.appendChild(newHeader);
